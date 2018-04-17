28285d7ee6f52e9b59ded3700f7928e0e2d5a805
limingzheWin <erdai1995@outlook.com>
2018-04-17 16:57:15 +0800

    4/17

---
+++ src/leetcode/LeetCode.java
@@ -0,0 +1,58 @@
+package leetcode;
+
+import java.util.HashMap;
+
+public class LeetCode {
+    /*
+    Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    Example:
+
+    Given nums = [2, 7, 11, 15], target = 9,
+
+    Because nums[0] + nums[1] = 2 + 7 = 9,
+    return [0, 1].
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                res[0] = map.get(target - nums[i]);
+                res[1] = i;
+                return res;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return res;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    /*
+    You are given two non-empty linked lists representing two non-negative integers.
+    The digits are stored in reverse order and each of their nodes contain a single digit.
+    Add the two numbers and return it as a linked list.
+
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+    Example
+
+    Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+    Output: 7 -> 0 -> 8
+    Explanation: 342 + 465 = 807.
+     */
+//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//
+//    }
+}
