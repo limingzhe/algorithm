e1a630d8e982893ed396d90fd176d3bb09d98b7b
limingzheWin <erdai1995@outlook.com>
2018-04-18 12:29:31 +0800

    4/18

--- src/leetcode/LeetCode.java
+++ src/leetcode/LeetCode.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class LeetCode {
     /*
@@ -15,9 +16,12 @@ public class LeetCode {
     Because nums[0] + nums[1] = 2 + 7 = 9,
     return [0, 1].
      */
+    /*
+    思路：hash表记录《值，下标》，对于当前数，如果 结果-自己 在哈希表里，说明符合条件，否则把自己加入哈希表
+     */
     public int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
-        HashMap<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(target - nums[i])) {
                 res[0] = map.get(target - nums[i]);
@@ -52,7 +56,50 @@ public class LeetCode {
     Output: 7 -> 0 -> 8
     Explanation: 342 + 465 = 807.
      */
-//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-//
-//    }
+    /*
+    思路：题目里是反向存储，设置一个进位即可，符合三个条件里任何一个while循环都需要进行下去
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = dummyHead;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
+            carry = sum / 10;
+            p.next = new ListNode(sum % 10);
+            p = p.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        return dummyHead.next;
+    }
+
+    /*
+    Given a string, find the length of the longest substring without repeating characters.
+
+    Examples:
+
+    Given "abcabcbb", the answer is "abc", which the length is 3.
+
+    Given "bbbbb", the answer is "b", with the length of 1.
+
+    Given "pwwkew", the answer is "wke", with the length of 3.
+    Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+     */
+    /*
+    最长不重复连续子串问题，使用滑动窗口，每次更新 前窗口，如果有重复的可能更新后窗口
+     */
+    public int lengthOfLongestSubstring(String s) {
+        int ans = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        int lo = 0;
+        for (int hi = 0; hi < s.length(); hi++) {
+            if (map.containsKey(s.charAt(hi))) {
+                lo = Math.max(lo, map.get(s.charAt(hi)) + 1);
+            }
+            ans = Math.max(hi - lo + 1, ans);
+            map.put(s.charAt(hi), hi);
+        }
+        return ans;
+    }
 }
--- src/problem/KcolorInNposition.java
+++ src/problem/KcolorInNposition.java
@@ -3,6 +3,9 @@ package problem;
 import java.util.Scanner;
 
 public class KcolorInNposition {
+    /*
+    把k种颜色放进n个位置，且每种颜色至少占领一个位置
+     */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int N = scan.nextInt();
